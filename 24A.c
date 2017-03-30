0140d395acd59dbfd117be76f6bd4529e27ba95d
Caesar0m0d <Caesar0m0d@gmail.com>
2017-03-31 06:50:33 +0900

    24A.c

--- 24A.c
+++ 24A.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 	int main(){
-int n,l,l2,l3,u,v,w,cw,temp,ccw,pre=0,crt=1;
+int n,l,l2,l3,u,v,w,cw=0,temp,temp2,ccw=0,pre=0,crt=1;
 scanf("%d",&n);
 int ** arr = (int **)malloc(sizeof(int *)*n);
 		for(l=0;l<n;l++)arr[l] = (int *)calloc(4,sizeof(int));
@@ -14,22 +14,35 @@ scanf("%d %d %d",&u,&v,&w);
 		else{arr[v-1][1] = u;arr[v-1][3]=-1;}
 				}
 l=0;
+		//if((arr[l][2]>0)?(temp=2):((arr[l][3]>0)?(temp=3):0)){
+
 		do{
-		if((arr[l][2]>0)?(temp=2):((arr[l][3]>0)?(temp=3):0)){
+printf("crt: %d %d\n",crt,cw);
+		if((arr[l][2]>0) || (arr[l][3]>0)){
+		if((arr[l][2]<0) || (arr[l][3]<0)){
+(arr[l][0] == pre)?(temp2=1):((arr[l][1] == pre)?(temp2=0):0);
+if(arr[l][temp2+2]<0)
+{(arr[arr[l][temp2]-1][0] == crt)?(ccw += arr[arr[l][temp2]-1][2]):(ccw += arr[arr[l][temp2]-1][3]);printf("ccw1\n");}
+else if(arr[l][temp2+2]>0)
+{(cw += arr[l][3]);printf("cw1\n");}
+temp = temp2 + 2;
+						  }
 		if((arr[l][2]>0)&&(arr[l][3]>0))
-{(pre == arr[l][0])?(temp = 3):(temp = 2)}
-cw += arr[l][temp];pre=l+1;crt=arr[l][temp-2];
+{(pre == arr[l][0])?(temp = 3):(temp = 2);cw += arr[l][temp];printf("cw2\n");}
+pre=l+1;crt=arr[l][temp-2];
 								     }
-		else if((pre == arr[l][0])?(temp = 1):(temp = 0))
-{ccw += arr[arr[l][temp]-1][;pre=l+1;crt=arr[l][temp]}
+		else if((pre == arr[l][0])?(temp = 3):(temp = 2))
+{(arr[arr[l][temp-2]-1][0] == crt)?(ccw += arr[arr[l][temp-2]-1][2]):(ccw += arr[arr[l][temp-2]-1][3]);pre=l+1;crt=arr[l][temp-2];printf("ccw2\n");}
 //proto	(arr[next][0] == pre)?(ccw += arr[next][2]):(ccw += arr[next][3]);
 l=crt-1;
-		}while(crt == 1)
+		}while(crt != 1);
 
 //test
 for(l=0;l<n;l++){
 for(l2=0;l2<4;l2++){printf("%d ",arr[l][l2]);}
 printf("\n");}
 //test
+printf("cw: %d\n",cw);
+printf("ccw: %d\n",ccw);
 return 0;
 		  }
